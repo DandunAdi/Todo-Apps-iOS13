6a7a25263444b36db6a45b2865e431375f1ffb91
Dandun Adi <dandun.adi@gmail.com>
2020-06-25 21:29:27 +0700

    add category model

---
+++ Todo Apps/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Todo Apps
+//
+//  Created by DDD on 25/06/20.
+//  Copyright © 2020 Dandun Adi. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = "New Category"
+}
